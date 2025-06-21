798999e90df7f9bd86284704d67f616b92bc9586
alan-augusto <augustoalan56@gmail.com>
2025-06-21 12:34:47 -0300

    feat(prisma): add migration for default user profile picture

---
+++ prisma/migrations/20250621150154_set_default_picture_to_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "profile_picture" SET DEFAULT 'https://img.freepik.com/premium-psd/contact-icon-illustration-isolated_23-2151903357.jpg?semt=ais_hybrid&w=740';
